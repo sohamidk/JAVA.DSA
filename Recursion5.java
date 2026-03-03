47212030ece89c936defa00098aad49bbe016a65
SOham <soham.a.vade@gmail.com>
2026-03-03 21:08:39 +0530

    recurse 5

---
+++ Recursion5.java
@@ -0,0 +1,19 @@
+public class Recursion5{
+    public static int findXn(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+        int a = findXn(x, n-1);
+        int b = x * a;
+        return b;
+    }
+
+    public static void main(String args[]){
+        int ans = findXn(2, 3);
+        System.out.println(ans);
+    }
+    
+}
