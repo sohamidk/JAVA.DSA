6bfc50cd5fdbc830302be53f8feb0d7e50a8d30c
SOham <soham.a.vade@gmail.com>
2026-03-04 21:00:16 +0530

    Advance 1

---
+++ advRecursion1.java
@@ -0,0 +1,17 @@
+public class advRecursion1{
+    public static void towerOfHanoi(int n, String src, String helper, String dest){
+        if(n == 1){
+            System.out.println("Transfer " + n + " from " + src + " to " + dest);
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, helper);
+         System.out.println("Transfer " + n + " from" + src + " to " + dest);
+        towerOfHanoi(n-1, helper, src, dest);
+        
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        towerOfHanoi(n, "S", "H", "D");
+    }
+}
