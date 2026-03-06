aa9f08924b733c6eaf2b394cdd805b41cad99f1f
SOham <soham.a.vade@gmail.com>
2026-03-06 22:44:08 +0530

    Advance 5

--- advRecursion2.java
+++ advRecursion2.java
@@ -1,13 +1,13 @@
 public class advRecursion2 {
     //String reverse using recursion
-    public static void printRev(String str, int idx){
-     if(idx == 0){
-        System.out.print(str.charAt(idx) + " ");
-        return ;
-     }
-     System.out.println(str.charAt(idx)+ " ");
-     printRev(str, idx - 1);
+   public static void printRev(String str, int idx){
+    if(idx == 0){
+        System.out.print(str.charAt(idx)+ " ");
+        return;
     }
+    System.out.print(str.charAt(idx) + " ");
+    printRev(str, idx - 1);
+   }
     public static void main(String args[]){
         String str = "GitHub";
         printRev(str, str.length() - 1);
--- advRecursion3.java
+++ advRecursion3.java
@@ -1,28 +1,27 @@
 public class advRecursion3 {
+
         public static int first = -1;
         public static int last = -1;
-
+       
         public static void findOcc(String str, int idx, char element){
-            if(idx == str.length())
-            {
-                System.out.println(first);
-                System.out.println(last);
-                return;
-            }
-            char currChar = str.charAt(idx);
-            if(currChar == element){
-                if(first == -1){
-                    first = -1;
-                }else{
-                    last = -1;
-                }
-            }
-        
-            findOcc(str, idx + 1, element);
+        if(idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
         }
-    
-        public static void main(String args[]){
-                String str = "abaacdaaefaah";
-             findOcc(str, 0, 'a');
+        char currChar = str.charAt(idx);
+        if(currChar == element){
+            if(first == -1){
+                first = idx;
+            }
+            else{
+                last = idx;
+            }
         }
+        findOcc(str, idx+1, element);
+       }
+       public static void main(String args[]){
+        String str = "abaacdaaefa";
+        findOcc(str, 0, 'a');
+       }
 }
