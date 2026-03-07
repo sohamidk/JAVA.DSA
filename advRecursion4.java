721cc655562c134902781e3e9718da7f025b25da
SOham <soham.a.vade@gmail.com>
2026-03-07 20:52:37 +0530

    Advance 4

--- advRecursion4.java
+++ advRecursion4.java
@@ -1,21 +1,25 @@
 public class advRecursion4 {
-    public static void moveAllX(String str, int idx, int count, String newString){
-        if(idx == str.length()){
-            System.out.println(newString);
-            return;
-        }
+        public static void moveAllX(String str, int idx, int count, String newString){
 
-        char currChar = str.charAt(idx);
-        if(currChar == 'x'){
-            count++;
-            moveAllX(str, idx + 1, count, newString);
-        }else{
-            newString += currChar;
-            moveAllX(str, idx+1, count, newString);
+            if(idx == str.length()){
+                for(int i = 0; i< count; i++){
+                    newString += 'x';
+                    
+                }
+                System.out.println(newString);
+                return;
+            }
+            char currChar = str.charAt(idx);
+            if(currChar == 'x'){
+                count++;
+                moveAllX(str, idx+1, count, newString);
+            }
+            else{
+                newString += currChar;
+                moveAllX(str, idx + 1, count, newString);
+            }
         }
-    }
-
-    public static void main(String args[]){
+       public static void main(String args[]){
         String str = "axbcxxd";
         moveAllX(str, 0, 0, "");
     }
